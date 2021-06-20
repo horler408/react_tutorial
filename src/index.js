18b1f1dc0ae6c629aa8b6757d42b271946119e30
horler408 <horler.web@gmail.com>
2021-06-20 23:15:40 +0100

    props lesson

--- src/index.js
+++ src/index.js
@@ -3,14 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 
+// const title = 'I love you to the Moon and Back';
+// const img ='https://www.amazon.com/Last-Thing-He-Told-Me/dp/1501171348/ref=zg_bs_books_3?_encoding=UTF8&psc=1&refRID=3EGGGSPAXRH67MG5EBQZ';
+
+const firstBook = {
+  title: 'The President Daughter',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/71bGKnfgZtL._AC_UL200_SR200,200_.jpg',
+  author: 'James Perterson',
+};
+
+const secondBook = {
+  title: 'Hail Mary',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/91Bd7P8UwxL._AC_UL200_SR200,200_.jpg',
+  author: 'Andy Weir',
+};
+
+const thirdBook = {
+  title: 'I love you to the Moon and Back',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+  author: 'Amelia Hepworth',
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
@@ -19,23 +45,21 @@ const Greeting = () => {
   return React.createElement('h1', {}, 'hello world');
 };
 
-const Book = () => {
+const Book = ({ img, author, title }) => {
+  // console.log(props);
+  // const { img, title, author } = props;
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt="The banner of the book" />
+      <h1> {title} </h1>;
+      <h4
+        style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}
+      >
+        {author}
+      </h4>
     </article>
   );
 };
-
-const Image = () => <img src="" alt="The banner of the book" />;
-const Title = () => <h1> I love you to the moon and back</h1>;
-const Author = () => (
-  <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
-    Yusuf Olaitan
-  </h4>
-);
 
 ReactDOM.render(
   <React.StrictMode>
